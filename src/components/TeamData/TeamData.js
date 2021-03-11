5b48b5d4add7114d6fd77321c33636f7051c3255
NAZMUS SAKIB <sakibabir52@gmail.com>
2021-03-11 18:52:17 +0600

    Social links updated

--- src/components/TeamData/TeamData.js
+++ src/components/TeamData/TeamData.js
@@ -12,7 +12,7 @@ import Gender from '../../Icon/gender.png';
 
 
 const TeamData = (props) => {
-    const { strTeam, intFormedYear, strCountry, strGender, strDescriptionEN, strTeamBanner, strSport } = props.teamData;
+    const { strTeam, intFormedYear, strCountry, strGender, strDescriptionEN, strTeamBanner, strFacebook, strTwitter, strYoutube, strSport } = props.teamData;
     
     let isMale;
     if(strGender === "Male"){
@@ -43,9 +43,9 @@ const TeamData = (props) => {
                 <p>{strDescriptionEN}</p>
                 <br />
                 <div className="social-icons">
-                    <a href="https://twitter.com/" target="_blank"><FontAwesomeIcon className="font-icon twitter" icon={faTwitter}/></a>
-                    <a href="https://www.facebook.com/" target="_blank"><FontAwesomeIcon className="font-icon" icon={faFacebook}/></a>
-                    <a href="https://www.youtube.com/" target="_blank"><FontAwesomeIcon className="font-icon youtube" icon={faYoutube}/></a>
+                    <a href={`https://${strTwitter}`} target="_blank"><FontAwesomeIcon className="font-icon twitter" icon={faTwitter}/></a>
+                    <a href={`https://${strFacebook}`} target="_blank"><FontAwesomeIcon className="font-icon" icon={faFacebook}/></a>
+                    <a href={`https://${strYoutube}`} target="_blank"><FontAwesomeIcon className="font-icon youtube" icon={faYoutube}/></a>
                 </div>
             </div>
 
